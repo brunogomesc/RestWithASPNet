4a529a81c4850665500a205795620a82f1df5e59
Bruno Costa <brunogomesc@outlook.com.br>
2021-07-28 11:52:55 -0300

    Implementing HATEOAS

---
+++ 12_RestWithASPNet_HATEOAS/v1_RestWithASPNet/RestWithASPNet/Data/VO/BookVO.cs
@@ -0,0 +1,25 @@
+﻿using RestWithASPNet.Hypermedia;
+using RestWithASPNet.Hypermedia.Abstract;
+using RestWithASPNet.Model.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithASPNet.Data.VO
+{
+    public class BookVO : ISupportsHyperMedia
+    {
+
+        public long Id { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime LaunchDate { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Title { get; set; }
+
+        public List<HyperMediaLink> Links { get; set; } = new List<HyperMediaLink>();
+    }
+}
