acd9897392385ca34f3e4a13a39c15e1f9f1e247
Bruno Costa <brunogomesc@outlook.com.br>
2021-08-01 18:48:06 -0300

    Implementing CORS

---
+++ 14_RestWithASPNet_CORS/v1_RestWithASPNet/RestWithASPNet/Model/Book.cs
@@ -0,0 +1,25 @@
+﻿using RestWithASPNet.Model.Base;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithASPNet.Model
+{
+    //Class identifying the columns in the database
+    [Table("books")]
+    public class Book : BaseEntity
+    {
+
+        [Column("author")]
+        public string Author { get; set; }
+
+        [Column("launch_date")]
+        public DateTime LaunchDate { get; set; }
+
+        [Column("price")]
+        public decimal Price { get; set; }
+
+        [Column("title")]
+        public string Title { get; set; }
+
+    }
+}
