01d471e760ec23301b8514b9a8a62becda3b2304
Bruno Costa <brunogomesc@outlook.com.br>
2021-08-03 23:18:31 -0300

    Implementing method for refresh infos the user

--- 15_RestWithASPNet_Autenthication/v1_RestWithASPNet/RestWithASPNet/db/migrations/V5__Create_Table_Users.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE users (
-id int primary key identity,
-users varchar(255) unique not null default '0',
-pass varchar(100) not null default '0',
-full_name varchar(300) not null,
-refresh_token varchar(500) not null default '0',
-refresh_token_expiry_time datetime default null
-)
